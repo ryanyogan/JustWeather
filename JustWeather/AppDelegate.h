14fb7da154c932ea0f4c7ac5c3a17a8a043b7adb
Ryan Yogan <ryanyogan@gmail.com>
2015-10-21 10:58:01 -0500

    Initial Commit

---
+++ JustWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JustWeather
+//
+//  Created by Ryan Yogan on 10/21/15.
+//  Copyright © 2015 Ryan Yogan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
