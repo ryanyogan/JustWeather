38e324dacd9e16a7051c3bc44456ae17fecdf6aa
Ryan Yogan <ryanyogan@gmail.com>
2015-10-21 11:51:00 -0500

    Added a basic controller, override the initial finish launching with a new rootViewController and out WXControllerView

---
+++ WXController.h
@@ -0,0 +1,13 @@
+//
+//  WXController.h
+//  JustWeather
+//
+//  Created by Ryan Yogan on 10/21/15.
+//  Copyright © 2015 Ryan Yogan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WXController : UIViewController
+
+@end
