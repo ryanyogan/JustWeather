493aebaa557b0bc2284a3651cd41f6980b87016e
Ryan Yogan <ryanyogan@gmail.com>
2015-10-21 13:40:22 -0500

    added in JSON to obj-c mappings w/ Mantle

--- WXCondition.h
+++ WXCondition.h
@@ -8,6 +8,24 @@
 
 #import <Mantle/Mantle.h>
 
-@interface WXCondition : MTLModel
+@interface WXCondition : MTLModel <MTLJSONSerializing>
+
+// Data mapping properties from JSON response
+@property (nonatomic, strong) NSDate *date;
+@property (nonatomic, strong) NSNumber *humidity;
+@property (nonatomic, strong) NSNumber *temperature;
+@property (nonatomic, strong) NSNumber *tempHigh;
+@property (nonatomic, strong) NSNumber *tempLow;
+@property (nonatomic, strong) NSString *locationName;
+@property (nonatomic, strong) NSDate *sunrise;
+@property (nonatomic, strong) NSDate *sunset;
+@property (nonatomic, strong) NSString *conditionDecription;
+@property (nonatomic, strong) NSString *condition;
+@property (nonatomic, strong) NSNumber *windBearing;
+@property (nonatomic, strong) NSNumber *windSpeed;
+@property (nonatomic, strong) NSString *icon;
+
+// Public accessor for imageName
+- (NSString *)imageName;
 
 @end
