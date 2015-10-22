05db58723b3d102b280eef9514d5f8d878030661
Ryan Yogan <ryanyogan@gmail.com>
2015-10-21 19:59:29 -0500

    implemented the url fetcher and current conditions object mapper

--- WXClient.m
+++ WXClient.m
@@ -7,7 +7,67 @@
 //
 
 #import "WXClient.h"
+#import "WXCondition.h"
+#import "WXDailyForecast.h"
+
+// Private interface
+@interface WXClient ()
+@property (nonatomic, strong) NSURLSession *session;
+@end
 
 @implementation WXClient
+
+- (id)init {
+    if (self = [super init]) {
+        NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+        _session = [NSURLSession sessionWithConfiguration:config];
+    }
+    return self;
+}
+
+- (RACSignal *)fetchCurrentConditionsForLocation:(CLLocationCoordinate2D)coordinate {
+    NSString *urlString = [NSString stringWithFormat:@"http://api.openweather.org/data/2.5/weather? \
+                           lat=%f&lon=%f&units=imperial", coordinate.latitude, coordinate.longitude];
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    return [[self fetchJSONFromURL:url] map:^(NSDictionary *json) {
+        return [MTLJSONAdapter modelOfClass:[WXCondition class] fromJSONDictionary:json error:nil];
+    }];
+                           
+}
+
+- (RACSignal *)fetchJSONFromURL:(NSURL *)url {
+    NSLog(@"Fetching: %@", url.absoluteString);
+    
+    return [[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        NSURLSessionDataTask *dataTask = [self.session dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+            
+            if (! error) {
+                NSError *jsonError = nil;
+                id json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&jsonError];
+                if (! jsonError) {
+                    [subscriber sendNext:json];
+                }
+                else {
+                    [subscriber sendError:jsonError];
+                }
+            }
+            else {
+                [subscriber sendError:error];
+            }
+            
+            [subscriber sendCompleted];
+            
+        }];
+        
+        [dataTask resume];
+        
+        return [RACDisposable disposableWithBlock:^{
+            [dataTask cancel];
+        }];
+    }] doError:^(NSError *error) {
+        NSLog(@"%@", error);
+    }];
+}
 
 @end
